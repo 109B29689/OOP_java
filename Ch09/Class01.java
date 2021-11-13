bd52cad86fb1b3a877843e8f15034c3592002099
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-11-13 10:36:39 +0800

    Update Class01.java

--- Ch09/Class01.java
+++ Ch09/Class01.java
@@ -1,1 +1,36 @@
+class CRectangle {
+  int width;
+  int height;
 
+  public CRectangle(int w, int h) {
+    width = w;
+    height = h;
+    System.out.println("CRectangle(int w, int h)呼叫");
+  }
+
+  public CRectangle() {
+    this(10, 8);
+    System.out.println("CRectangle()呼叫，由this()生成");
+  }
+
+  public void show() {
+    System.out.println("width=" + width + " height=" + height);
+  }
+}
+
+public class Class01 {
+  public static void main(String[] args) {
+    CRectangle cr1 = new CRectangle(20, 16);
+    cr1.show();
+    CRectangle cr2 = new CRectangle();
+    cr2.show();
+  }
+}
+
+/* output-------------
+CRectangle(int w, int h)呼叫
+width=20 height=16
+CRectangle(int w, int h)呼叫
+CRectangle()呼叫，由this()生成
+width=10 height=8
+--------------------*/
