1d151eb07cbd57f07337615ec572cc9dfcff5fed
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-11-13 11:36:55 +0800

    Update Class07.java

--- Ch09/Class07.java
+++ Ch09/Class07.java
@@ -1,1 +1,21 @@
+class Math {
+  public static void power(int x, int n) {
+    int Ans = 1;
+    for (int i = 1; i <= n; i++) {
+      Ans *= x;
+    }
+    System.out.println("power(int x, int n)呼叫,x=" + x + " n=" + n + " Ans=" + Ans);
+  }
+}
 
+public class Class07 {
+  public static void main(String[] args) {
+    Math.power(2, 5);
+    Math.power(3, 2);
+  }
+}
+
+/* output-------------
+power(int x, int n)呼叫,x=2 n=5 Ans=32
+power(int x, int n)呼叫,x=3 n=2 Ans=9
+--------------------*/
