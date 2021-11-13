de203798012dccbc2d188964f33237f28e5f8048
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-11-13 16:53:28 +0800

    Update Class09.java

--- CH10/Class09.java
+++ CH10/Class09.java
@@ -1,3 +1,45 @@
-public class Class09 {
 
+class Caaa {
+  private int num;
+
+  public Caaa(int n) {
+    num = n;
+  }
+
+  public int get() {
+    return num;
+  }
+
+  public void display() {
+    System.out.println("printed from Caaa class");
+  }
 }
+
+class Cbbb extends Caaa {
+  public Cbbb(int n) {
+    super(n);
+  }
+
+  public void display() {
+    super.display();
+    System.out.println("printed from Cbbb class");
+  }
+
+  public void show() {
+    System.out.println("num=" + get());
+  }
+}
+
+public class Class09 {
+  public static void main(String[] args) {
+    Caaa bb = new Cbbb(0);
+    bb.display();
+    System.out.println("兩者都會呼叫，父類別先，子類別後");
+  }
+}
+
+/* output-------------
+printed from Caaa class
+printed from Cbbb class
+兩者都會呼叫，父類別先，子類別後
+--------------------*/
