4cb0926cb61ea21734e2cbcc25fac9eaabe5003f
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-01 18:13:31 +0800

    Create Class09.java

---
+++ Ch08/Class09.java
@@ -0,0 +1,41 @@
+class CCalculator{
+  int a;
+  int b;
+  int c;
+  public void set_value(int x,int y,int z){
+    a=x;
+    b=y;
+    c=z;
+  }
+  public int add(){
+    int add=(a+b+c);
+    return add;
+  }
+  public int sub(){
+    int sub=(a-b-c);
+    return sub;
+  }
+  public int mul(){
+    int mul=(a*b*c);
+    return mul;
+  }
+  public double avg(){
+    double avg=(sub()/2.0);
+    System.out.println("hi");
+    return avg;
+  }
+  public void show(){
+    System.out.println("a="+a+" b="+b+" c="+c+" add="+add()+" sub="+sub()+" mul="+mul()+" avg="+avg());
+  }
+}
+class Main {  
+  public static void main(String args[]) {
+    CCalculator obj=new CCalculator();
+    obj.set_value(25,3,7);
+    obj.show();
+  } 
+}
+
+/*output------------------------------------
+a=25 b=3 c=7 add=35 sub=15 mul=525 avg=7.5
+------------------------------------------*/
