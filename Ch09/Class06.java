fe381dbbe5f82b404575aae17653d8d7e2dd8933
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-02 13:01:56 +0800

    Create Class06.java

---
+++ Ch09/Class06.java
@@ -0,0 +1,19 @@
+class Caaa{
+  public void add2n(int n){
+    if(n%2==0) System.out.println((n+1)*n/2);
+   else System.out.println((((n-1)/2)*(n+1))+(n+1)/2);
+  }
+}
+class Main {  
+  public static void main(String args[]) {
+    Caaa ca1=new Caaa();
+    Caaa ca2=new Caaa();
+    ca1.add2n(5);
+    ca2.add2n(10);
+  }
+}
+
+/*output---
+15
+55
+---------*/
