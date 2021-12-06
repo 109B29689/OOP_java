332d9c319dff8f9e5eb489207faa7b8c68375d93
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-06 11:45:44 +0800

    Create Class09.java

---
+++ ch12/pack09/Class09.java
@@ -0,0 +1,14 @@
+package pack09;
+import pack09.sub1.CShape;
+import pack09.sub2.CTranpezoid;
+public class Class09 {
+    public double pi=3.14;
+    public double radius;
+    public int upper;
+    public int base;
+    public int height;
+    public void main(String args[]){
+        CShape cs=new CShape(2.0);
+        CTranpezoid ct=new CTranpezoid(1,2,3);
+    }
+}
