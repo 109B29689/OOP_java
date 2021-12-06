5203e997d56b8e992abbe6d96ca14dd5ba2c351e
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-06 20:32:54 +0800

    Update Class08.java

--- Ch12/pack08/Class08.java
+++ Ch12/pack08/Class08.java
@@ -1,1 +1,17 @@
+package pack08;
+import pack08.subpack1.CCircle;
+import pack08.subpack1.subsubpack.CStyle;
+public class Class08
+{
+   public static void main(String args[])
+   {
+      CCircle cir=new CCircle();
+      cir.show();
+      CStyle sty=new CStyle(1);
+   }
+}
 
+/*output--------------------------
+show() in class CCircle called
+style1
+--------------------------------*/
