ee71602d59bbcb6569b611939694a9674494ffd1
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-06 14:51:00 +0800

    Update Class06.java

--- ch10/Class06.java
+++ ch10/Class06.java
@@ -1,1 +1,33 @@
+class CTriangle {
+  protected int base;
+  protected int height;
 
+  protected void show() {
+    System.out.println("base=" + base + ", height=" + height);
+  }
+}
+
+class CData extends CTriangle {
+  public CData(int b, int h) {
+    super();
+    base = b;
+    height = h;
+  }
+
+  public void area() {
+    show();
+    System.out.println("area=" + base * height / 2.0);
+  }
+}
+
+public class Class06 {
+  public static void main(String[] args) {
+    CData obj = new CData(3, 8);
+    obj.area();
+  }
+}
+
+/* output-------------
+base=3, height=8
+area=12.0
+--------------------*/
