679a9d9a3be3a2142cb0a14bf9c2ad4ca085435d
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-09 13:12:05 +0800

    Create Class14.java

---
+++ Ch11/Class14.java
@@ -0,0 +1,31 @@
+interface iVolume{
+  public void showData();
+  public double vol();
+}
+abstract class CShape implements iVolume{
+  final double pi=3.14;
+  protected int x;
+  protected int y;
+}
+class CCircle extends CShape{
+  private int r;
+  public CCircle(int X,int Y,int R){
+    x=X;
+    y=Y;
+    r=R;
+  }
+  public double vol(){
+    return (r*r*r*pi*4/3);
+  }
+  public void showData(){
+    System.out.println("球心：("+x+","+y+")");
+    System.out.println("半徑："+r);
+    System.out.println("球體積："+vol());
+  }
+}
+class Class14 {  
+  public static void main(String args[]) { 
+    CCircle cir=new CCircle(8,6,2);
+    cir.showData();
+  } 
+}
