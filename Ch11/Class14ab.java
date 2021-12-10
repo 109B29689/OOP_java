5749bcffea4f865156dd17981edaf453cd7f6be8
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-10 10:53:52 +0800

    Update Class14ab.java

--- Ch11/Class14ab.java
+++ Ch11/Class14ab.java
@@ -23,43 +23,6 @@ class CCircle extends CSphere{
     System.out.println("球體積："+vol());
   }
 }
-class Class14ab {  
-  public static void main(String args[]) { 
-    CCircle cir=new CCircle(8,6,2);
-    cir.showData();
-  } 
-}
-
-/*output-------------------
-球心：(8,5)
-半徑：2
-球體積：33.49333333333333
--------------------------*/
-interface iVolume{
-  public void showData();
-  public double vol();
-}
-abstract class CSphere implements iVolume{
-  final double pi=3.14;
-  protected int x;
-  protected int y;
-}
-class CCircle extends CSphere{
-  private int r;
-  public CCircle(int X,int Y,int R){
-    x=X;
-    y=Y;
-    r=R;
-  }
-  public double vol(){
-    return (r*r*r*pi*4/3);
-  }
-  public void showData(){
-    System.out.println("球心：("+x+","+y+")");
-    System.out.println("半徑："+r);
-    System.out.println("球體積："+vol());
-  }
-}
 class Main {  
   public static void main(String args[]) { 
     CCircle cir=new CCircle(8,6,2);
