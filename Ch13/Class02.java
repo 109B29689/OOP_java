a7187a7e3205089a10df68d1358a07d2c1990a06
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-12 15:06:25 +0800

    Create Class02.java

---
+++ Ch13/Class02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int num=12,den=sc.nextInt();
+    System.out.println("and of main()!!");
+    if(den==0) System.out.println("除數為0");
+    else System.out.println(num/den);
+  }
+}
+
+/*output---------------
+input 0
+and of main()!!
+除數為0
+
+input 3
+and of main()!!
+4
+---------------------*/
