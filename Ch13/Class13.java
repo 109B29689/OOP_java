aea8af3b6006f1e1a93d72dd00c76c7c20fa27a5
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-13 12:00:44 +0800

    Create Class13.java

---
+++ Ch13/Class13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class StringException extends Exception{
+}
+public class Class13{
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		try {
+			if(a.equals("520")) throw new StringException();
+			else System.out.println(a);
+		}
+		catch(StringException b) {
+			System.out.println("這是由字串520所引起的例外");
+		}
+	}
+}
+
+/*output-----
