cf147746d632808f1e8ec7826aa0fb5ca4336724
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-17 13:05:21 +0800

    Update Class09.java

--- Ch12/pack09/Class09.java
+++ Ch12/pack09/Class09.java
@@ -1,14 +1,16 @@
 package pack09;
-import pack09.sub1.CShape;
+import pack09.sub1.CSphere;
 import pack09.sub2.CTranpezoid;
 public class Class09 {
-    public double pi=3.14;
-    public double radius;
-    public int upper;
-    public int base;
-    public int height;
-    public void main(String args[]){
-        CShape cs=new CShape(2.0);
+    public static void main(String args[]){
+        CSphere cs=new CSphere(2.0);
         CTranpezoid ct=new CTranpezoid(1,2,3);
+        cs.show();
+        ct.show();
     }
 }
+
+/*output--------------------
+Shphere=33.49333333333333
+Trapezoid=4
+--------------------------*/
