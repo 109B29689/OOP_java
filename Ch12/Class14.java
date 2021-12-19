1757ff20630f3c7d79b3751b8157e29d47c405c0
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-19 15:23:16 +0800

    Create Class14.java

---
+++ Ch12/Class14.java
@@ -0,0 +1,10 @@
+class Class14 {  
+  public static void main(String args[]) { 
+    String a="Habit is second nature.";
+    System.out.println(a.substring(16,22));
+  } 
+}
+
+/*output---
+nature
+---------*/
