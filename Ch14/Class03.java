be87b2e9b770c656a8ece3a76633eef1f1b94812
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-20 11:07:06 +0800

    Add files via upload

---
+++ Ch14/Class03.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Class03 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String a=br.readLine();
+        int j=0,k=Integer.parseInt(a);
+        for(int i=1;i<=k;i++) j+=i;
+        System.out.println(j);
+    }
+}
