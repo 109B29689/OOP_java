4e4fb69e4faf773290eebd914faaee163871d718
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-20 11:06:45 +0800

    Create Class02.java

---
+++ Ch14/Class02.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Class02 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println((sc.nextLine()).toUpperCase());
+    }
+}
+
+/*output-------------------
+input Knowldege is power.
+KNOWLDEGE IS POWER.
+-------------------------*/
