8dc84a0ac197cf7cec2785c10b1e9a5da614a22b
109B29689 <91858061+109B29689@users.noreply.github.com>
2022-01-05 08:42:09 +0800

    Update Class08.java

--- Ch14/Class08.java
+++ Ch14/Class08.java
@@ -1,13 +1,13 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
-public class Main
+public class Class08
 {
    public static void main(String args[]) throws IOException
    {
-     FileReader fra=new FileReader("aaa.txt");
-     FileReader frb=new FileReader("bbb.txt");
-     FileWriter fwc=new FileWriter("ccc.txt");
+     FileReader fra=new FileReader("Ch14\\aaa.txt");
+     FileReader frb=new FileReader("Ch14\\bbb.txt");
+     FileWriter fwc=new FileWriter("Ch14\\ccc.txt");
      char ca[]=new char[32],cb[]=new char[32];
      int a=fra.read(ca),b=frb.read(cb);
      String sa=new String(ca,0,a),sb=new String(cb,0,b);
