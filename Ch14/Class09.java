8e2b1b47d8469de56d14237edaafcaee88c0de1b
109B29689 <91858061+109B29689@users.noreply.github.com>
2022-01-05 22:27:46 +0800

    Update Class09.java

--- Ch14/Class09.java
+++ Ch14/Class09.java
@@ -4,9 +4,9 @@ public class Class09
 {
    public static void main(String args[]) throws IOException
    {
-     FileReader fra=new FileReader("aaa.txt");
-     FileReader frb=new FileReader("bbb.txt");
-     FileReader frc=new FileReader("ccc.txt");
+     FileReader fra=new FileReader("Ch14\\aaa.txt");
+     FileReader frb=new FileReader("Ch14\\bbb.txt");
+     FileReader frc=new FileReader("Ch14\\ccc.txt");
      char ca[]=new char[32],cb[]=new char[32],cc[]=new char[64];
      int a=fra.read(ca),b=frb.read(cb),c=frc.read(cc);
      String sa=new String(ca,0,a),sb=new String(cb,0,b),sc=new String(cc,0,c);
